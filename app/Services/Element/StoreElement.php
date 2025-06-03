0ba73403f725e6bd8a04569ed386c771d18b2f9d
thirithantdede <thirithant.hs@gmail.com>
2025-06-04 02:18:30 +0700

    DEV :CODE UPDATE

--- app/Services/Element/StoreElement.php
+++ app/Services/Element/StoreElement.php
@@ -70,6 +70,9 @@ class StoreElement
 
     public function updateStoreInfo(ShopInformation $shopElement, array $data)
     {
+        $shopElement->element->update([
+            'name' => $data['name'],
+        ]);
         $shopElement->update($data);
 
         return $shopElement;
