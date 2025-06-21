44ae1cd1c916db8c6605c2495e879cfe6f306920
thirithantdede <thirithant.hs@gmail.com>
2025-06-22 02:14:07 +0700

    dev : dashboard service enhance

--- app/Services/Dashboard/DashboardService.php
+++ app/Services/Dashboard/DashboardService.php
@@ -2,14 +2,22 @@
 
 namespace App\Services\Dashboard;
 
+use App\Models\Analytics;
+use App\Models\Element;
+use App\Models\ElementType;
 use App\Models\Project;
 use App\Models\ProjectView;
+use App\Services\Element\BaseElement;
 
 class DashboardService
 {
 
-    public function __construct(public Project $project, public string $startDate, public string $endDate) {}
+    protected string $startDate;
+    protected string $endDate;
 
+    public function __construct(public Project $project,public string $dateType) {
+        $this->setTimePeriod($dateType);
+    }
 
     public function setTimePeriod(string $type = "day"){
         switch($type){
@@ -25,11 +33,43 @@ class DashboardService
                 $this->startDate = now()->startOfMonth();
                 $this->endDate = now()->endOfMonth();
                 break;
+            case "year":
+                $this->startDate = now()->startOfYear();
+                $this->endDate = now()->endOfYear();
+                break;
         }
     }
 
-    public function getTotalViews(): int
+    public function getRouteSearch():int
     {
-        return ProjectView::count();
+        return Analytics::query()->where('event_type','from-route')
+        ->where('project_id','=', $this->project->id)
+        ->whereBetween('created_at', [$this->startDate, $this->endDate])
+        ->count();
+    }
+
+    public function getActiveStores() :int
+    {        
+        $types = ElementType::where('is_store',true)->first();
+        return Element::where('element_type_id', $types->id)
+        ->where('project_id','=', $this->project->id)
+        ->whereBetween('created_at', [$this->startDate, $this->endDate])
+        ->count();
+    }
+
+    public function getTotalVisitors()
+    {
+        return ProjectView::where("project_id","=", $this->project->id)
+            ->whereBetween('created_at', [$this->startDate, $this->endDate])
+            ->count();
+    }
+    
+    public function getTotalEvents(): int
+    {
+        $eventElementType = ElementType::where('name','event')->first();
+        return Element::where('element_type_id','=', $eventElementType->id)
+        ->where('project_id','=', $this->project->id)
+        ->whereBetween('created_at', [$this->startDate, $this->endDate])
+        ->count();
     }
 }
